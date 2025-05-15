d486fa96a39e1ac2f0165211d25bb676d1c8e126
daffpy <105541452+daffpy@users.noreply.github.com>
2025-05-15 13:07:42 +0700

    validation login
    contoh validation untuk login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,23 @@
 import { login } from "../actions/actions"
 import Link from "next/link"
+import {listErrorLogin} from "@/scripts/utility"
 
-export default function LoginPage(){
+export default async function LoginPage({searchParams}){
+    let error = {};
+    const sp = await searchParams
+    const errorData = sp.error
+
+    if (errorData !== undefined) {
+        try {
+            error = JSON.parse(decodeURIComponent(errorData));
+            console.log(error)
+        } catch (e) {
+            console.error('Failed to parse error', e);
+        }
+    }
+
+
+
     return <div className="h-screen flex items-center justify-center">
         <div>
         <div className="text-center text-2xl">LOGIN</div>
@@ -9,11 +25,18 @@ export default function LoginPage(){
             <div>
                 <label className="block">Binusian Email</label>
                 <textarea name="email" placeholder="@binus.ac.id" style={{resize: "none"}} className=" border p-2 h-10 mt-2 rounded-lg text-sm"></textarea>
+                {//<label className="block mb-4 italic text-red-500">{error.email ? error.email : ""}</label>
+                }
             </div>
             <div>
             <label className="block"> Password</label>
                 <textarea name="password" style={{resize: "none"}} className="border p-2 h-10 mt-2 rounded-lg text-sm"></textarea>
             </div>
+            {Object.entries(error).map(([field,e], idx) => (
+                <div key={idx}>
+                    {field}: {e}
+                </div>
+            ))}
             <button type="submit" className="cursor-pointer border p-2 mt-5 text-sm rounded-lg w-20 hover:bg-white hover:text-black">Submit</button>
             <Link href="/register">
                 <div className="mt-5 italic">
